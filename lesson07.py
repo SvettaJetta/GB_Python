cf372fbb6a25c8dd75c038419bd892dcf73ff482
Svetlana Nikolaeva <34101243+SvettaJetta@users.noreply.github.com>
2022-11-18 20:33:09 +0300

    Update lesson07.py

--- lesson07.py
+++ lesson07.py
@@ -2,31 +2,30 @@ import logging
 import os
 import sympy as sym
 
-
 NUMBER_TYPE = {
-    "Rational": 1,
-    "Complex": 2,
-    "Exit": 0
+    1: "Rational",
+    2: "Complex",
+    0: "Exit"
 }
 
 OPERATIONS_NUMBER = {
-    "Plus": 1,  # Сложение
-    "Multiplication": 2,  # Умножение
-    "Division": 3,  # Деление
-    "Subtraction": 4,  # Вычитание
-    "Exit": 0
+    1: "Plus",
+    2: "Multiplication",
+    3: "Division",
+    4: "Subtraction",
+    0: "Exit"
 }
 
 
 def logger(a, b, operant):
     logging.info(f"The values of a and b are {a} and {b}.")
     try:
-        logging.info(f"a, b successful with result.")
+        logging.info(f"a, b successful with operator {operant}.")
     except ZeroDivisionError as err:
         logging.error("ZeroDivisionError", exc_info=True)
 
 
-def rational_operation(selected_operator):
+def rational_operation(selected_operator,selected_type):
     # numerator = int(input("Numerator is:"))
     # denominator = int(input("Denominator is:"))
 
@@ -42,24 +41,22 @@ def rational_operation(selected_operator):
             print("")
             rational_operation(selected_operator)
 
-        if selected_operator == ("1" or "+" or "plus"):
-            return plus(a, b,selected_operator)
-        if selected_operator == ("2" or "*" or "multiplication"):
-            return multiplication(a, b,selected_operator)
-        if selected_operator == ("3" or "/" or "Division"):
-            return division(a, b,selected_operator)
-        if selected_operator == ("4" or "-" or "subtraction"):
-            return subtraction(a, b, selected_operator)
+        if selected_operator == "plus":
+            return plus(a, b, selected_type)
+        if selected_operator == "multiplication":
+            return multiplication(a, b, selected_type)
+        if selected_operator == "division":
+            return division(a, b, selected_type)
+        if selected_operator == "subtraction":
+            return subtraction(a, b, selected_type)
 
         else:
-            rational_operation(selected_operator)
+            rational_operation(selected_operator,selected_type)
 
+        logger(a, b, selected_operator)
 
-
-
-def complex_operation(selected_operator):
-
-    if selected_operator == "0" or "exit":
+def complex_operation(selected_operator, selected_type):
+    if selected_operator == "exit":
         exit()
     else:
         global a, b
@@ -69,98 +66,99 @@ def complex_operation(selected_operator):
 
         except:
             print("")
-            rational_operation(selected_operator)
+            rational_operation(selected_operator,selected_type)
 
-
-
-        if selected_operator == ("1" or "+" or "plus"):
-            return plus(a, b,selected_operator)
-        if selected_operator == ("2" or "*" or "multiplication"):
-            return multiplication(a, b,selected_operator)
-        if selected_operator == ("3" or "/" or "division"):
-            return division(a, b,selected_operator)
-        if selected_operator == ("4" or "-" or "subtraction"):
-            return subtraction(a, b,selected_operator)
-        if selected_operator == ("0" or "exit"):
+        if selected_operator == "plus":
+            return plus(a, b, selected_type)
+        if selected_operator == "multiplication":
+            return multiplication(a, b, selected_type)
+        if selected_operator == "division":
+            return division(a, b, selected_type)
+        if selected_operator == "subtraction":
+            return subtraction(a, b, selected_type)
+        if selected_operator == "exit":
             exit()
         else:
-            complex_operation(selected_operator)
+            complex_operation(selected_operator, selected_type)
 
-        logger(a,b , selected_operator)
+        logger(a, b, selected_operator )
 
 
 def plus(a, b, op):
     global result
-    if op == ("2" or "complex"):
-        result = complex(int(a[0]),int(a[1]))+complex(int(b[0]),int(b[1]))
-    if op == ("1" or "rational"):
-        result = sym.Rational(int(a[0]), int(a[1])) + sym.Rational(int(b[0]), int(b[1]))
-    return result
+    if op == "complex":
+        return complex(int(a[0]), int(a[1])) + complex(int(b[0]), int(b[1]))
+    if op == "rational":
+        return sym.Rational(int(a[0]), int(a[1])) + sym.Rational(int(b[0]), int(b[1]))
+
+
 
 
 def multiplication(a, b, op):
     global result
-    if op == ("2" or "complex"):
-        result = complex(int(a[0]),int(a[1]))*complex(int(b[0]),int(b[1]))
-    if op == ("1" or "rational"):
-        result = sym.Rational(int(a[0]), int(a[1])) * sym.Rational(int(b[0]), int(b[1]))
+    if op == "complex":
+        return complex(int(a[0]), int(a[1])) * complex(int(b[0]), int(b[1]))
+    if op == "rational":
+        return sym.Rational(int(a[0]), int(a[1])) * sym.Rational(int(b[0]), int(b[1]))
 
-    return result
+
 
 
 def division(a, b, op):
     global result
-    if op == ("2" or "complex"):
-        result =complex(int(a[0]),int(a[1]))/complex(int(b[0]),int(b[1]))
-    if op == ("1" or "rational"):
-        result = sym.Rational(int(a[0]), int(a[1])) / sym.Rational(int(b[0]), int(b[1]))
-    return result
+    if op == "complex":
+        return  complex(int(a[0]), int(a[1])) / complex(int(b[0]), int(b[1]))
+    if op == "rational":
+        return  sym.Rational(int(a[0]), int(a[1])) / sym.Rational(int(b[0]), int(b[1]))
+
 
 
 def subtraction(a, b, op):
     global result
-    if op == ("2" or "complex"):
-        result = complex(int(a[0]),int(a[1]))-complex(int(b[0]),int(b[1]))
-    if op == ("1" or "rational"):
-        result = sym.Rational(int(a[0]), int(a[1])) - sym.Rational(int(b[0]), int(b[1]))
-    return result
+    if op == "complex":
+        return complex(int(a[0]), int(a[1])) - complex(int(b[0]), int(b[1]))
+    if op == "rational":
+        return sym.Rational(int(a[0]), int(a[1])) - sym.Rational(int(b[0]), int(b[1]))
+
 
 
 def print_operations():
     print("Operations Menu")
     for x in OPERATIONS_NUMBER:
-        print('\t', OPERATIONS_NUMBER[x], x)
-    selected_operator = input("Input operator: ")
+        print('\t', x, OPERATIONS_NUMBER[x])
+    selected_operator = int(input("Input operator: "))
 
-    return selected_operator.lower()
+    return str(OPERATIONS_NUMBER[selected_operator]).lower()
 
 
 def print_types():
     print("Types Menu")
     for x in NUMBER_TYPE:
-        print('\t', NUMBER_TYPE[x], x)
-    selected_type = input("Input Type: ")
+        print('\t', x, NUMBER_TYPE[x])
+    selected_type = int(input("Input Type: "))
 
-    return selected_type.lower()
+    return str(NUMBER_TYPE[selected_type]).lower()
+
 
 def menu():
     selected_type = print_types()
+    print("Selected type is: ",selected_type)
 
-
-    if selected_type == ("1" or "rational"):
+    if selected_type == "rational":
         my_operator = print_operations()
-        print(rational_operation(my_operator))
+        print("Operator: ", my_operator)
+        print(rational_operation(my_operator, selected_type))
 
-
-    if selected_type == ("2" or "complex"):
+    if selected_type == "complex":
         my_operator = print_operations()
-        print(complex_operation(my_operator))
+        print("Operator: ", my_operator)
+        print(complex_operation(my_operator, selected_type))
 
-    if selected_type == ("0" or "exit"):
+    if selected_type == "exit":
         exit()
     else:
         print("")
-        #menu()
+        # menu()
 
 
 if __name__ == '__main__':
